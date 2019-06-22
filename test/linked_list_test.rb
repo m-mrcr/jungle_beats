1ae300a3a6ade886aa4754db6e4876e353a6040f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-06-22 16:11:43 -0600

    iterations 2-4

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -17,6 +17,7 @@ class LinkedListTest < MiniTest::Test
   def test_it_can_append_to_head
     @list.append("doop")
     assert_instance_of Node, @list.head
+    assert_equal "doop", @list.head.data
   end
 
   def test_it_can_display_count_of_single_appended_node
@@ -27,6 +28,84 @@ class LinkedListTest < MiniTest::Test
   def test_it_can_display_attribute_of_single_node_as_part_of_string
     @list.append("doop")
     assert_equal "doop", @list.to_string
+  end
+
+  def test_it_can_functionally_have_multiple_nodes
+    @list.append("doop")
+    assert_nil @list.head.next_node
+    @list.append("deep")
+    assert_nil @list.head.next_node.next_node
+    assert_equal "deep", @list.head.next_node.data
+  end
+
+  def test_it_can_return_count_for_multiple_nodes
+   @list.append("doop")
+   assert_equal 1, @list.count
+   @list.append("deep")
+   assert_equal 2, @list.count
+  end
+
+  def test_it_can_convert_multiple_nodes_to_string
+    @list.append("doop")
+    assert_equal "doop", @list.to_string
+    @list.append("deep")
+    assert_equal "doop deep", @list.to_string
+  end
+
+  def test_it_can_prepend_data
+    @list.append("plop")
+    assert_equal "plop", @list.to_string
+    @list.append("suu")
+    assert_equal "plop suu", @list.to_string
+    @list.prepend("dop")
+    assert_equal "dop plop suu", @list.to_string
+    assert_equal 3, @list.count
+  end
+
+  def test_it_can_insert_data_given_an_index
+    @list.append("plop")
+    assert_equal "plop", @list.to_string
+    @list.append("suu")
+    assert_equal "plop suu", @list.to_string
+    @list.prepend("dop")
+    assert_equal "dop plop suu", @list.to_string
+    @list.insert(1, "woo")
+    assert_equal "dop woo plop suu", @list.to_string
+    assert_equal 4, @list.count
+  end
+
+  def test_it_can_find_find_by_index_and_num_of_nodes
+    @list.append("deep")
+    @list.append("woo")
+    @list.append("shi")
+    @list.append("shu")
+    @list.append("blop")
+    assert_equal "deep woo shi shu blop", @list.to_string
+    assert_equal "shi", @list.find(2, 1)
+    assert_equal "woo shi shu", @list.find(1, 3)
+  end
+
+  def test_it_can_tell_whether_it_includes_a_given_value
+    @list.append("deep")
+    @list.append("woo")
+    @list.append("shi")
+    @list.append("shu")
+    @list.append("blop")
+    assert @list.includes?("deep")
+    refute @list.includes?("dep")
+  end
+
+  def test_pop_removes_last_word
+    @list.append("deep")
+    @list.append("woo")
+    @list.append("shi")
+    @list.append("shu")
+    @list.append("blop")
+    assert_equal "deep woo shi shu blop", @list.to_string
+    @list.pop
+    assert_equal "deep woo shi shu", @list.to_string
+    @list.pop
+    assert_equal "deep woo shi", @list.to_string
   end
 
 end
